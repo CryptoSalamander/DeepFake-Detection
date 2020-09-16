ec94da7970ccac63d6096b23b2955580f59b1fb5
Hyunsoo Luke HA <dhy03196@naver.com>
2020-09-16 16:21:11 +0900

    [MOD] Add LogLoss Function

--- eval.py
+++ eval.py
@@ -2,8 +2,11 @@ import argparse
 import json
 import os
 import csv
+import math
+import numpy as np
 from glob import glob
 from pathlib import Path
+from sklearn.metrics import log_loss
 
 total = 0
 correct = 0
@@ -11,7 +14,6 @@ true_positive = 0
 true_negative = 0
 false_positive = 0
 false_negative = 0
-
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser("Evaluate Models")
@@ -24,6 +26,8 @@ help="Real Threshold")
     args = parser.parse_args()
     FAKE_thres = args.fake_threshold
     REAL_thres = args.real_threshold
+    y = []
+    y_pred = []
     with open(args.answer_json) as json_file:
         json_data = json.load(json_file)
         for csv_path in glob(os.path.join(args.result_path, "*.csv")):
@@ -35,12 +39,16 @@ help="Real Threshold")
                     total += 1 
                     json_object = json_data[line[0]]
                     if json_object['label'] == 'FAKE':
+                        y.append(1)
+                        y_pred.append(float(line[1]))
                         if float(line[1]) >= FAKE_thres:
                             correct += 1
                             true_positive += 1
                         else:
                             false_positive += 1
                     elif json_object['label'] == 'REAL':
+                        y.append(0)
+                        y_pred.append(float(line[1]))
                         if float(line[1]) < REAL_thres:
                             correct += 1
                             true_negative += 1
@@ -53,3 +61,4 @@ help="Real Threshold")
     print('Recall\t\t', recall)
     print('F1 Score\t', 2*(precision * recall) / (precision + recall))
     print('Fall-out\t', false_positive / (true_negative + false_positive))
+    print('Log-Loss\t', log_loss(y,y_pred))
